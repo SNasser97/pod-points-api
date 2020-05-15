be0952799947611ce6f5e41a96b7edeccf238f87
SNasser97 <snasserahmed@outlook.com>
2020-05-15 20:15:20 +0100

    replace key string with env var dev key

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require("express");
+const fetch = require("node-fetch");
 const cors = require("cors");
 const app = express();
 
@@ -9,22 +11,26 @@ app.get("/", (req, res) => {
 	res.send("working!");
 })
 
-
 app.get("/testRoute", (req,res)=> {
-	// let key = "5bc09b556c3f4175b51bfef06a27543c";
-	// let url = "https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0";
-	// // req.get("X-ListenAPI-Key", "5bc09b556c3f4175b51bfef06a27543c");
-	// // res.json();
-	// fetch(url, {
-	// 	method:"GET",
-	// 	"X-ListenAPI-Key":key
-	// }).then(resp => {
-	// 	// console.log(resp.data.status);
-	// 	// res.send(res.data.status);
-	// }).catch(err=>console.log(err));
-	// res.set("X-ListenAPI-Key", "5bc09b556c3f4175b51bfef06a27543c");
-	// res.send;
+	let url =
+		"https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0";
+	fetch(url, {
+		headers: {
+			"Content-Type": "application/json",
+			"X-ListenAPI-Key": `${process.env.API_KEY}`
+		}
+	})
+	.then(resp => resp.json())
+	.then(data => {
+		console.log(data);
+		res.send(data);
+	})
+	.catch(console.error);
+	
 })
 
-app.listen(3001);
-console.log("App is running on 3001...");
+const PORT = process.env.SERVER_PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`App running on ${PORT}...`);
+});
+
