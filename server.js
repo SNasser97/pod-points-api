197c41d4358b3fb8e076be4c2661113ee78b35e8
SNasser97 <snasserahmed@outlook.com>
2020-05-16 16:08:34 +0100

    routes for external api calls random and episode list using node-fetch

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.get("/", (req, res) => {
 	res.send("working!");
-})
+});
 
-app.get("/testRoute", (req,res)=> {
-	let url =
-		"https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0";
+//! /just_listen route (random episode)
+app.get("/random_episode", (req,res)=> {
+	let url = (
+		"https://listen-api.listennotes.com/api/v2/just_listen"
+		);
+
 	fetch(url, {
 		headers: {
 			"Content-Type": "application/json",
@@ -21,16 +24,31 @@ app.get("/testRoute", (req,res)=> {
 		}
 	})
 	.then(resp => resp.json())
-	.then(data => {
-		console.log(data);
-		res.send(data);
-	})
+	.then(data => res.json(data))
 	.catch(console.error);
-	
 })
 
+//! /listen
+app.post("/episodes", (req, res) => {
+	fetch(req.body.url, {
+		headers: {
+			"Content-Type":"application/json",
+			"X-ListenAPI-Key": `${process.env.API_KEY}`
+		}
+	})
+	.then(resp => resp.json())
+	.then(data => res.json(data))
+	.catch(err=> console.log);
+})
 const PORT = process.env.SERVER_PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`App running on ${PORT}...`);
 });
 
+// // TODO: ROUTE - /episode_random - GET
+// // TODO: ROUTE - /episode
+// TODO: ROUTE - /signin - POST
+// TODO: ROUTE - /register - POST
+// TODO: ROUTE - /profile - GET
+// TODO: ROUTE - /leaderboard - GET
+// TODO: ROUTE - /score - PUT
