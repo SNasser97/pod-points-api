0ab201056b1544775c57ab7f916961d225f3d14a
SNasser97 <snasserahmed@outlook.com>
2020-06-22 20:34:56 +0100

    add dummy data to check input from client-side

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}));
 app.get("/", (req, res) => {
 	res.send("working!");
 });
@@ -16,7 +16,7 @@ app.get("/random_episode", (req,res)=> {
 	let url = (
 		"https://listen-api.listennotes.com/api/v2/just_listen"
 		);
-
+	
 	fetch(url, {
 		headers: {
 			"Content-Type": "application/json",
@@ -46,10 +46,49 @@ app.post("/episodes", (req, res) => {
 	.catch(err => console.error(err));
 });
 
+// dummy data
+const test_db = [
+	{
+		id: 1,
+		username:'DoesPods12',
+		password:'pods321',
+		email: 'jdoe@example.com',
+		score: 200,
+		joined: new Date()
+	},
+	{
+		id: 2,
+		username: 'MaggieA',
+		password: 'cookies1',
+		email: 'maggie@gmail.com',
+		score: 1220,
+		joined: new Date()
+	}
+]
+
+app.post('/sign_in', (req, res) => {
+
+	// console.log('req', req)
+	const username = req.body.username
+	const password = req.body.password
+	console.log('BODY', req.body)
+	// console.log(req.query)
+	// console.log('test')
+	console.log('QUERY', req.query)
+	if (test_db[1].username === username && test_db[1].password === password) {
+		console.log('SUCCESS =>', {username, password})
+		res.json(test_db[1])
+	} else {
+		console.log('FAILED =>', {username, password})
+		res.status(400).send()
+	}
+})
+
 const PORT = process.env.SERVER_PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`App running on ${PORT}...`);
 });
+
 
 // // TODO: ROUTE - /episode_random - GET
 // // TODO: ROUTE - /episode
