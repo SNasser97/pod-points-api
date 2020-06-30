a9603bb27e63fcd71423cb0d9c609789bca07a95
SNasser97 <snasserahmed@outlook.com>
2020-06-30 13:01:10 +0100

    remove commented code + console log output

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,27 +1,19 @@
 const handleSignIn = (req, res, db, hash='temp') => {
   const username = req.body.username
   const password = req.body.password
-  // let targetUser = [];
-  console.log('db of users', db);
+  
+  console.log('db of users', db); //! for debugging - remove
   console.log('BODY=>', req.body)
   
-  const user = db.findIndex(acc => {
+  const index = db.users.findIndex(acc => {
     return acc.username === username && acc.password === password ? acc : ''
   })
-  console.log(user)
-  if(db[user]) {
-    res.json(db[user])
+  // check if such user exists.
+  if (db.users[index]) {
+    res.json(db.users[index])
   } else {
     res.status(400).send('Incorrect username or password')
   }
-  // console.log('trgt', targetUser)
-  // if (targetUser) {
-  //   console.log('SIGNING IN... =>', targetUser)
-  //   res.json(targetUser)
-  // } else {
-  //   console.log('INCORRECT USER OR PASS =>', targetUser)
-  //   res.status(400).send()
-  // }
 }
 
 module.exports = {
