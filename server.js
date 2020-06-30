57f80251f83ff05bfdf074c37de614bfc37bcbc1
SNasser97 <snasserahmed@outlook.com>
2020-06-30 12:59:08 +0100

    serve html page on root + import controllers and uuid

--- server.js
+++ server.js
@@ -8,52 +8,65 @@ const signIn = require("./controllers/signIn");
 const register = require("./controllers/register");
 const episodes = require("./controllers/episodes");
 const randomEpisodes = require("./controllers/randomEpisodes");
+const profile = require("./controllers/profile");
+const score = require("./controllers/score");
+
+const { uuid } = require('uuidv4');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:false}));
 app.get("/", (req, res) => {
-	res.send("working!");
+	res.sendFile(__dirname+"/index.html");
 });
 
 // dummy data
-const test_db = [
-	{
-		id: 1,
-		username:'DoesPods12',
-		password:'pods321',
-		email: 'jdoe@example.com',
-		score: 200,
-		joined: new Date()
-	},
-	{
-		id: 2,
-		username: 'MaggieA',
-		password: 'cookies1',
-		email: 'maggie@gmail.com',
-		score: 1220,
-		joined: new Date()
-	}
-]
-// requests
+const test_db = {
+	users: [
+		{
+			id: "1",
+			username: 'DoesPods12',
+			password: 'pods321',
+			email: 'jdoe@example.com',
+			score: 200,
+			joined: new Date()
+		},
+		{
+			id: "2",
+			username: 'MaggieA',
+			password: 'cookies1',
+			email: 'maggie@gmail.com',
+			score: 1220,
+			joined: new Date()
+		}
+	]
+}
+//* main requests
 app.post('/sign_in', (req, res) => {
 	signIn.handleSignIn(req,res, test_db);
 });
-
 app.post('/register', (req, res) => {
 	register.handleRegister(req,res,test_db);
 });
+app.get("/profile/:id", (req, res) => {
+	profile.handleProfileGet(req,res, test_db);
+});
+app.put("/score", (req, res) => {
+	score.handleScoreUpdate(req, res, test_db);
+});
 
+//* vendor requests
+app.post("/episodes", (req, res) => {
+	episodes.getEpisodesAPI(req, res, fetch)
+});
 app.get("/random_episode", (req, res) => {
 	randomEpisodes.getRandomEpisodesAPI(req, res, fetch)
 });
 
-app.post("/episodes", (req, res) => {
-	episodes.getEpisodesAPI(req, res, fetch)
-});
 
-const PORT = process.env.SERVER_PORT || 3001;
+
+const PORT = process.env.SERVER_PORT || 3002;
 app.listen(PORT, () => {
 	console.log(`App running on ${PORT}...`);
 });
