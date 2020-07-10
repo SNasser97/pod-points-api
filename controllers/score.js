226521c9072a9155c1bc1fdb9aa0790be0035dce
SNasser97 <snasserahmed@outlook.com>
2020-07-10 15:21:27 +0100

    query for updating user score by random incrementation

--- controllers/score.js
+++ controllers/score.js
@@ -1,18 +1,21 @@
 
 const handleScoreUpdate = (req, res, db) => {
   const { id } = req.body;
-  const reward = Math.floor(Math.random() * 250) + 70;
+  const generateRandReward = () => Math.floor(Math.random() * 250) + 75
   
-  const index = db.users.findIndex(user => user.id === id);
-
-  if(db.users[index]) {
-    db.users[index].score += reward;
-    console.log('user =>', db.users[index])
-    console.log('you\'ve been awarded ' + reward + ' points!');
-    res.json({user_score_new:db.users[index].score, reward});
-  } else {
-    res.status(400).send('user not found, cannot update score');
-  }
+  // match id from body to that in pgsql
+  db.select("*").from("Users")
+      .where("id", "=", id)
+      .increment("score", generateRandReward())
+      .returning("score")
+      .then(score => {
+        if (score.length) {
+          return res.json(score[0]) // param score returns arr of score as str ['100']
+        } else {
+          throw new Error("score not found"); // error message we throw to catch if else condition met
+        }
+      })
+      .catch(error => res.status(400).json(error.message))
 }
 
 module.exports = {
