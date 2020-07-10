2c3a69a5f651200316c013fbfcf7e60aedcf01c0
SNasser97 <snasserahmed@outlook.com>
2020-07-10 15:20:53 +0100

    handle user sign in and compare input to hashed string

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,17 +1,28 @@
-const handleSignIn = (req, res, db, hash='temp') => {
-  const username = req.body.username
-  const password = req.body.password
+const handleSignIn = (req, res, db, bcrypt) => {
+  const {username, password} = req.body
   
-  console.log('BODY=>', req.body)
-  
-  const index = db.users.findIndex(acc => {
-    return acc.username === username && acc.password === password ? acc : ''
-  })
-  // check if such user exists.
-  if (db.users[index]) {
-    res.json(db.users[index])
+  if (username && password) {
+    db.select("*").from("Login")
+      .where("username", "=", username)
+      .then(data => {
+        const [user] = data;
+        const isHashValid = bcrypt.compareSync(password, user.hash);
+
+        if (user.username && isHashValid) {
+          db.select("*").from("Users").where("id", "=", user.id)
+          .then(user => res.json({
+            id: user[0].id, 
+            username: user[0].username, 
+            score: user[0].score, 
+            joined: user[0].joined
+          }));
+        } else {
+          throw new Error("Incorrect username or password");
+        }
+      })
+      .catch(err => res.status(400).json(err.message))
   } else {
-    res.status(400).send('Incorrect username or password')
+    res.status(400).json("Please enter your details");
   }
 }
 
