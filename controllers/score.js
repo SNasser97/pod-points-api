b29349d7c25129a25b809f35521d92480ebce331
SNasser97 <snasserahmed@outlook.com>
2020-07-16 14:57:17 +0100

    increment user's score by providing randomised value between 75 and 250 returning props 'reward' + 'user_new_score' as JSON

--- controllers/score.js
+++ controllers/score.js
@@ -1,16 +1,17 @@
 
 const handleScoreUpdate = (req, res, db) => {
   const { id } = req.body;
-  const generateRandReward = () => Math.floor(Math.random() * 250) + 75
-  
+  const MAX_POINT = 250;
+  const MIN_POINT = 75;
+  const reward = Math.floor(Math.random() * (MAX_POINT - MIN_POINT + 1) + MIN_POINT) // generate score between 75 and 250
   // match id from body to that in pgsql
   db.select("*").from("Users")
       .where("id", "=", id)
-      .increment("score", generateRandReward())
+      .increment("score", reward)
       .returning("score")
       .then(score => {
         if (score.length) {
-          return res.json(score[0]) // param score returns arr of score as str ['100']
+          return res.json({reward, user_score_new:parseInt(score[0])}) // param score returns arr of score as str ['100']
         } else {
           throw new Error("score not found"); // error message we throw to catch if else condition met
         }
