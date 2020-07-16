aaf1bab9424a294bf3de7ead7d2a33d49fed2dc3
SNasser97 <snasserahmed@outlook.com>
2020-07-16 14:58:56 +0100

    create func to handle API req to Listen Notes vendor

---
+++ controllers/asyncHandler.js
@@ -0,0 +1,20 @@
+
+// handle simple requests to Listen Notes API
+const asyncHandler = async (req, res, fetch, fetch_url) => {
+  try {
+    const respData = await fetch(fetch_url, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-ListenAPI-Key": `${process.env.API_KEY}`
+      }
+    });
+    const respJSON = await respData.json();
+    res.json(respJSON);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+}
+
+module.exports = {
+  asyncHandler
+}
--- controllers/episodes.js
+++ controllers/episodes.js
@@ -1,18 +1,30 @@
-const getEpisodesAPI = (req,res, fetch) => {
-  // search param + offset sent from clientside (body of request)
-  const urlOffset = req.body.urlOffset;
-  const urlSearch = req.body.urlSearch;
-  const url = `https://listen-api.listennotes.com/api/v2/search?q=${urlSearch}&offset=${urlOffset ? urlOffset : 0}&scope=episode&language=Any language&len_min=0`
+const tryCatch = require(`${__dirname}/asyncHandler`);
 
-  fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-      "X-ListenAPI-Key": `${process.env.API_KEY}`
-    }
-  })
-  .then(resp => resp.json())
-  .then(data => res.json(data))
-  .catch(err => console.error(err));
+const getEpisodesAPI = async (req, res, fetch) => {
+  // search param + offset sent from clientside (body of request)
+  const {urlSearch, urlOffset} = req.body;
+  const url = `https://listen-api.listennotes.com/api/v2/search?q=${urlSearch}&offset=${urlOffset ? urlOffset : 0}&scope=episode&language=Any language&len_min=0`
+  
+  if (urlSearch !== "") {
+    tryCatch.asyncHandler(req, res, fetch, url);
+  } else {
+    res.status(400).json("Empty search query provided");
+  }
+  
+  
+  
+  // try {
+  //   const respData = await fetch(url, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "X-ListenAPI-Key": `${process.env.API_KEY}`
+  //     }
+  //   });
+  //   const respJSON = await respData.json();
+  //   res.json(respJSON);
+  // } catch (error) {
+  //   res.status(400).json(error.message);
+  // }
 }
 
 module.exports = {
