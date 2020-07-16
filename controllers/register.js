51bc40566ab0ad0abd5df9e3e4040e99e2a6f5f0
SNasser97 <snasserahmed@outlook.com>
2020-07-16 14:59:47 +0100

    handle basic validation on user reg + signin. Return error prop to handle client-side validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,23 @@
 const { uuid } = require("uuidv4");
 
+// validate email address with regEx
+const validateEmail = (email) => {
+  const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return reg.test(String(email).toLowerCase());
+}
+
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, username, password } = req.body
-
-  if (email !== "" && username !== "" && password !== "") {
+  // check if username or password for registration is valid
+  if (!email && !username && !password) {
+    res.status(400).json({error:"Please fill all fields"})
+  } else if (!validateEmail(email)) {
+    res.status(400).json({error:"Provide valid email address!"});
+  } else if (password.length <= 5) {
+    res.status(400).json({error:"Your password must be longer than 5 characters!"})
+  } else if (username.length <= 3) {
+    res.status(400).json({error:"Your username must be longer than 3 characters!"});
+  } else if (email !== "" && username !== "" && password !== "") {
     // make a transaction where we create a new user
     db.transaction(trx => {
       return trx.insert({
@@ -12,33 +26,35 @@ const handleRegister = (req, res, db, bcrypt) => {
         email,
         joined: new Date(),
       }).into("Users")
-      .returning(["id", "username"])
-      .then(LoginUser => {
-        return trx.select("*").from("Users")
-        .returning("*")
-        // return and insert User column id and username to Login
-        .insert({
-          id: LoginUser[0].id,
-          username: LoginUser[0].username,
-          hash: bcrypt.hashSync(password, 8)
-        })
-        .into("Login")
-        .then(user => {
-          if(user.length) {
-            // if new user exists make query to User table and retrieve new row
-            db.select("*").from("Users").where("id", "=", user[0].id)
-              .then(newUser => res.json(newUser[0]))
-          } else {
-            throw new Error("Could not register user")
-          }
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)  
+        .returning(["id", "username"])
+        .then(LoginUser => {
+            return trx.select("*")
+            .from("Users")
+            .returning("*") // return and insert User column id and username to Login
+            .insert({
+              id: LoginUser[0].id,
+              username: LoginUser[0].username,
+              hash: bcrypt.hashSync(password, 8)
+            })
+            .into("Login")
+            .then(user => {
+              if(user.length) {
+                // if new user exists make query to User table and retrieve new row
+                db.select("*")
+                  .from("Users")
+                  .where("id", "=", user[0].id)
+                  .then(newUser => res.json(newUser[0]))
+              } else {
+                throw new Error("Could not register user")
+              }
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)  
       })
-      .catch(err => res.status(400).json(err.message))
+      .catch(err => res.status(400).json({error:"Username or Email already in use"}))
     })
   } else {
-    res.status(400).json("Please fill in your details");
+    res.status(400).json({error:"Please fill all fields"});
   }
 }
 module.exports = {
--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,7 +1,7 @@
 const handleSignIn = (req, res, db, bcrypt) => {
   const {username, password} = req.body
   
-  if (username && password) {
+  if (username !== "" || password !== "") {
     db.select("*").from("Login")
       .where("username", "=", username)
       .then(data => {
@@ -9,20 +9,22 @@ const handleSignIn = (req, res, db, bcrypt) => {
         const isHashValid = bcrypt.compareSync(password, user.hash);
 
         if (user.username && isHashValid) {
-          db.select("*").from("Users").where("id", "=", user.id)
-          .then(user => res.json({
-            id: user[0].id, 
-            username: user[0].username, 
-            score: user[0].score, 
-            joined: user[0].joined
-          }));
+          return db.select("*")
+              .from("Users")
+              .where("id", "=", user.id)
+              .then(user => res.json({
+                id: user[0].id, 
+                username: user[0].username, 
+                score: user[0].score, 
+                joined: user[0].joined
+              }));
         } else {
           throw new Error("Incorrect username or password");
         }
       })
-      .catch(err => res.status(400).json(err.message))
+      .catch(err => res.status(400).json({error:err.message}))
   } else {
-    res.status(400).json("Please enter your details");
+    res.status(400).json({error:"Fill in missing details"});
   }
 }
 
