ef852cb489d315b769f15dbb1bf46d828ba62b67
SNasser97 <snasserahmed@outlook.com>
2020-07-16 14:57:47 +0100

    return id from database and provide JSON resp containing each user's position on leaderboard

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,12 +1,15 @@
 const handleGetLeaderboard = (req, res, db) => {
 
   // Order list of users by their current scores
-  db.select(["username", "score"]).from("Users").orderBy("score", "desc")
-  .then(listOfUsers => {
-    return res.json(listOfUsers);
-  })
-  .catch(err => res.status(400).json(err.message))
-
+  db.select(["id","username", "score"]).from("Users").orderBy("score", "desc")
+    .then(listOfUsers => {
+      // assign prop position to each user.
+      const eachUser = listOfUsers.map((user,i) => {
+        return  {...user, position:i+1}
+      })
+      return res.json(eachUser);
+    })
+    .catch(err => res.status(400).json(err.message))
 }
 
 module.exports = {
