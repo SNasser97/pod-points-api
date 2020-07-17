e8df850d8df0663453df42294299461986f2d0e3
SNasser97 <snasserahmed@outlook.com>
2020-07-17 13:24:53 +0100

    remove async from route + remove commentented code

--- controllers/randomEpisodes.js
+++ controllers/randomEpisodes.js
@@ -1,6 +1,6 @@
 const tryCatch = require(`${__dirname}/asyncHandler`);
 
-const getRandomEpisodesAPI = async (req,res,fetch) => {
+const getRandomEpisodesAPI = (req,res,fetch) => {
   const url = "https://listen-api.listennotes.com/api/v2/just_listen";
   tryCatch.asyncHandler(req,res,fetch,url);
 }
